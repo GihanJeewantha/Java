66e66190f76a53d88747e6e0a3da6720a16b54cf
GihanJeewantha <106438829+GihanJeewantha@users.noreply.github.com>
2022-08-08 14:19:15 +0530

    Java
    java basic

---
+++ FirstJavaProgramme/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is My First java code !");
+		System.out.println("This is another print statement ");
+		System.out.println("you want print \\ need to add two \\ other than it gives error ");
+		
+		//this is comment 
+		/* this is multiline comment*/
+
+	}
+
+}
