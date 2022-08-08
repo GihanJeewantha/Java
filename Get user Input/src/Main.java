266a0cc83e48344abc23776b7c118764bb567468
GihanJeewantha <106438829+GihanJeewantha@users.noreply.github.com>
2022-08-08 20:32:51 +0530

    Input from usser
    This is Ussing Scanner class get Usser input

---
+++ Get user Input/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner; //import Scanner class
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner Sc = new Scanner(System.in); //create Scanner class object
+		
+		System.out.println("What is you'r Name : ");
+		String name = Sc.nextLine();  // ussing that Scanner
+		
+		System.out.println("Enter you'r Age :");
+		int age = Sc.nextInt();
+		
+		System.out.println("Enter Your Heigth : ");
+		Float height = Sc.nextFloat();
+		
+		System.out.println("Your name is " + name);
+		System.out.println("Your age is : "+ age);
+		System.out.println("You'r Height is : "+ height);
+		
+			
+
+	}
+
+}
