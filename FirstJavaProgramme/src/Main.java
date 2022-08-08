5010cd70a29fddfe0c89e4dab7c4c952a5019134
GihanJeewantha <106438829+GihanJeewantha@users.noreply.github.com>
2022-08-08 20:05:49 +0530

    Java variable
    This is  java Variable

--- FirstJavaProgramme/src/Main.java
+++ FirstJavaProgramme/src/Main.java
@@ -7,7 +7,7 @@ public class Main {
 		System.out.println("This is another print statement ");
 		System.out.println("you want print \\ need to add two \\ other than it gives error ");
 		
-		//this is comment 
+		//this is single line comment 
 		/* this is multiline comment*/
 
 	}
---
+++ variable/src/variable/Main.java
@@ -0,0 +1,40 @@
+package variable;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int x; //declaration
+		
+		x = 123; //assignment
+		
+		int a = 456; //initialization
+		
+		System.out.println(x);
+		
+		System.out.println("Variable Number is " +a);
+		
+		double y = 3.14;
+		float c = (float) 12.50;
+		
+		System.out.println("this is Double variable " + y);
+		System.out.println("This is  Float variable " + c);
+		
+		boolean z = true;
+		boolean f = false;
+		
+		System.out.println("This is boolean " +z);
+		System.out.println("This is another boolean " +f);
+		
+		char symbol = '@';
+		char anotherSymbol = '$';
+		
+		//allway when we use char  data type  store data use single cortasion
+		System.out.println("This is char " +symbol);
+		System.out.println("This is another Char " + anotherSymbol);
+		
+		String name = "Jeewantha Dilshan";
+		System.out.println("Hello My name is " + name );
+	}
+
+}
