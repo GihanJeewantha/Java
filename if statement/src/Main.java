6f16190f1d2b8c39fff8112cce198f137857b875
GihanJeewantha <106438829+GihanJeewantha@users.noreply.github.com>
2022-08-10 21:18:45 +0530

    if Statement
    IF statement in java programming

---
+++ if statement/src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int age = 80;
+		
+		if(age==18) {
+			System.out.println("You are an adult ! ");
+		}
+		else if(age>=75) {
+			System.out.println("Ok You are 75 year old ! ");
+		}
+		
+		else {
+			System.out.println("You are not an Adult ! ");
+		}
+
+	}
+
+}
