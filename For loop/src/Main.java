f5fd7e895e7ece359bd8973afac6f9ab711326ea
GihanJeewantha <106438829+GihanJeewantha@users.noreply.github.com>
2022-08-11 20:05:21 +0530

    Update Main.java
    change for loop

--- For loop/src/Main.java
+++ For loop/src/Main.java
@@ -3,15 +3,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		for(int i =0; i<=10; i++) {
+		for(int i =10; i>=0; i--) {
 			System.out.println("This is I : "+ i);
 			
 		}
 		
 		
-		for(int a=10; a==1; a--) {
-			System.out.println("This is A: "+ a);
-		}
+		
 
 	}
 
