3fdce8855bebc62fcf8296a17791dfba18dc0a7d
GihanJeewantha <106438829+GihanJeewantha@users.noreply.github.com>
2022-08-12 00:08:41 +0530

    Arrays

---
+++ Array/src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		String[] cars = {"Tesla","BMW","Audi","Bugadi"};
+
+		int[] numbers= {1,2,3,4,5};
+		
+		cars[0] = "Mustang";
+		
+		System.out.println(cars[0]);
+		System.out.println(cars[1]);
+		System.out.println(cars[2]);
+		System.out.println(cars[3]);
+		
+		System.out.println(numbers[0]);
+		System.out.println(numbers[1]);
+		System.out.println(numbers[2]);
+		System.out.println(numbers[3]);
+	}
+
+}
