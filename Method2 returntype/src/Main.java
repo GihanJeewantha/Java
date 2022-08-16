25ca4199ef12b435318c17cc5f25593d3c18846e
GihanJeewantha <106438829+GihanJeewantha@users.noreply.github.com>
2022-08-16 21:47:26 +0530

    Return type

---
+++ Method2 returntype/src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int x = 3;
+		int y = 4;
+		
+		int z = add(x,y);
+		
+		System.out.println("sum: "+z);
+
+	}
+	
+	static int add(int x , int y) {
+		
+		int z = x + y;
+		return z;
+		
+	}
+
+}
