83915690ea007215aaf0996be83ca0d926d8ad17
GihanJeewantha <106438829+GihanJeewantha@users.noreply.github.com>
2022-08-18 22:59:22 +0530

    change

--- OOP3 local and global variable/src/DiseRoller.java
+++ OOP3 local and global variable/src/DiseRoller.java
@@ -1,4 +1,15 @@
 
 public class DiseRoller {
+	
+	DiseRoller(){
+		Random random = new Random();
+		int number = 0;
+		roll(random,number);
+	}
+	
+	void roll(Random random,int number) {
+		number = random.nextInt(6)+1;
+		System.out.println(number);
+	}
 
 }
